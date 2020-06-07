681270d8cd62882562e92bf5cdc07cf198a29d0f
Ghero56 <arcigaguzmanfernando@gmail.com>
2020-06-06 22:24:20 -0500

    base

---
+++ LifeGame.py
@@ -0,0 +1,12 @@
+import pygame
+import numpy as np
+
+pygame.init()
+
+width,height = 1000,1000
+screen = pygame.display.set_mode((height , width))
+bg = 25,25,25
+screen.fill(bg)
+
+while True:
+    pass
