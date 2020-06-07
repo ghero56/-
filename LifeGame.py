ce1638ca33a32af630d8f8ff09014ed87395074d
Ghero56 <arcigaguzmanfernando@gmail.com>
2020-06-07 00:14:04 -0500

    Beta

--- LifeGame.py
+++ LifeGame.py
@@ -2,65 +2,70 @@ import pygame
 import numpy as np
 import time
 
+width,height = 700,700
+nX, nY = 50, 50
+dimX = width/nX
+dimY = height/nY
+
 pygame.init()
+screen = pygame.display.set_mode([height,width])
 
-width,height = 1000,1000
+bg = (25,25,25)
+live_color = (255,255,255)
+dead_color = (128,128,128)
+# cell state 1=life,0=death
+gameState = np.zeros((nX,nY))
 
-# main screen
-screen = pygame.display.set_mode((height , width))
+pause = False
 
-# background
-bg = 25 , 25 , 25
-screen.fill(bg)
-
-# Cells
-nxC , nyC = 25 , 25
-
-# dimension
-dimCW = width/nxC
-dimCH = height/nyC
-
-# cell state 1=life , 0=death
-gameState = np.zeros((nxC , nyC))
-
+ciclo = True
 # cicle
-while True:
-
+while ciclo:
     newGameState = np.copy(gameState)
 
-    screen.fill(bg)
-    time.sleep(0.1)
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            pause = not pause
 
-    for y in range(0,nxC):
-        for x in range(0,nyC):
-            # near neighborgs
-            n_neigh  =  gameState[(x-1)%nxC , (y-1)%nyC] + \
-                        gameState[(x)%nxC , (y-1)%nyC] + \
-                        gameState[(x+1)%nxC , (y-1)%nyC] + \
-                        gameState[(x-1)%nxC , (y)%nyC] + \
-                        gameState[(x+1)%nxC , (y)%nyC] + \
-                        gameState[(x-1)%nxC , (y+1)%nyC] + \
-                        gameState[(x)%nxC , (y+1)%nyC] + \
-                        gameState[(x+1)%nxC , (y+1)%nyC]
+        if event.type == pygame.QUIT:
+            ciclo = False
 
-            # Rule 1 a death cell with exactly 3 neighborgs revives
-            if gameState[x , y] == 0 and n_neigh == 3:
-                newGameState[x , y] == 1
+        mouseClick = pygame.mouse.get_pressed()
+        if sum(mouseClick) > 0:
+            posX, posY = pygame.mouse.get_pos()
+            x, y = int(np.floor(posX/dimX)), int(np.floor(posY/dimY))
+            newGameState[x,y] = not mouseClick[2]
 
-            # Rule 2: a living cell with less than 2 or more than 3 neighborgs alive deaths
-            elif gameState[x , y] == 1 and (n_neigh < 2 or n_neigh > 3):
-                newGameState[x , y] == 0
 
-            poly = [((x)*dimCW, y*dimCH),
-                    ((x+1)*dimCW, y*dimCH),
-                    ((x+1)*dimCW, (y+1)*dimCH),
-                    ((x)*dimCW, (y+1)*dimCH)]
+    screen.fill(bg) # clean screen
 
-            if newGameState[x,y] == 0:
-                pygame.draw.polygon(screen , (128,128,128) , poly , 1)
+    for x in range(0,nX):
+        for y in range(0,nY):
+
+            if not pause:
+                # near neighborgs
+                n_neigh  =  gameState[(x-1)%nX,(y-1)%nY] + gameState[x%nX,(y-1)%nY] + \
+                            gameState[(x+1)%nX,(y-1)%nY] + gameState[(x-1)%nX,y%nY] + \
+                            gameState[(x+1)%nX,y%nY] + gameState[(x-1)%nX,(y+1)%nY]+\
+                            gameState[x%nX,(y+1)%nY] + gameState[(x+1)%nX,(y+1)%nY]
+
+                # Rule 1 a death cell with exactly 3 neighborgs revives
+                if gameState[x,y] == 0 and n_neigh == 3:
+                    newGameState[x,y] = 1
+
+                # Rule 2: a living cell with less than 2 or more than 3 neighborgs alive deaths
+                elif gameState[x,y] == 1 and (n_neigh < 2 or n_neigh > 3):
+                    newGameState[x,y] = 0
+
+            poly = [(x*dimX,y*dimY),((x+1)*dimX,y*dimY),((x+1)*dimX,(y+1)*dimY),(x*dimX,(y+1)*dimY)]
+
+            if newGameState[x,y] == 1:
+                pygame.draw.polygon(screen,live_color,poly,0)
             else:
-                pygame.draw.polygon(screen , (255,255,255) , poly , 0)
+                pygame.draw.polygon(screen,dead_color,poly,1)
 
     gameState = np.copy(newGameState)
-
+    time.sleep(0.1) # delay
     pygame.display.flip()
+
+pygame.quit()
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # -
+
+                    人生のゲーム
 Python repository for Life Game with pygame and numpy
